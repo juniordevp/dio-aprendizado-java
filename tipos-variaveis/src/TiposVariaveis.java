dc91619cab4057d55f9adda72983c70908a7b026
juniordevp <walterjunior66735@gmail.com>
2024-07-28 11:41:49 -0300

    Principais tipos de dados em java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.500;
+
+        String meuNome = "Walter Junior";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero  = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //Para determinarmos uma constante é necessário que utilizemos da key ´final´
+        final double VALOR_DE_PI = 3.14;
     }
 }
