1e2400b30b9ce401f9899f4ca5e54d84fc57fdb3
juniordevp <walterjunior66735@gmail.com>
2024-08-09 19:58:25 -0300

    execução sem a IDE

---
+++ execucao-fora-vscode/src/MinhaClasse.java
@@ -0,0 +1,20 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá, fui executado pelo terminal!");
+    }
+
+    /**
+     * Para realizarmos a execução do programa por fora da IDE,
+     * iremos seguir os seguintes passos:
+     * - Localizar o local que o nosso projeto está alocado
+     * (você pode fazer isso usando o explorador de arquivos)
+     * - Com o caminho do diretório copiado basta abrirmos o MS-DOS ou 
+     * PowerShell
+     * - Nele irá digitar: cd 'diretório do seu projeto'. No meu caso é
+     * cd D:\dio-aprendizado-java\execucao-fora-vscode
+     * - Dentro do diretório já, iremo digitar: 'cd bin' que é a pasta
+     * na qual está armazenado nossos arquivos já compilados.
+     * - Já dentro da pasta bin, basta digitar 'java NomeDoProjeto' no nosso
+     * exemplo 'java MinhaClasse'
+     */
+}
