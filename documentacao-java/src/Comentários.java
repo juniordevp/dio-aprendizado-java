3a0bbc4e203b24f1a03dd2d1b03aeac9c7a13bcc
juniordevp <walterjunior66735@gmail.com>
2024-08-09 19:10:39 -0300

    comentários recomendáveis e não recomendaveis

---
+++ documentacao-java/src/Comentários.java
@@ -0,0 +1,35 @@
+/**
+ * Este código tem com intuito demonstrar como se deve utilizar 
+ * os comentários durante o desenvolvimento.
+ * 
+ * @author Walter Junior
+ * @version 1.0
+ * @since 09/08/2024
+ */
+
+public class Comentários {
+    public static void main(String[] args) {
+        //Olá eu sou um comentário ema uma única linha
+
+        /*
+         * Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+
+    /**
+     * Estes dois asteriscos acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação
+     */
+    public void metodo() {
+
+    }
+
+    /* Não posso escrever meu código para ser dependente de comentários para ser compreendido,
+     os comentários devem ter apenas uma breve explicação de como ou para que serve aquele 
+     bloco de código */
+}
